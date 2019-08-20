1d6444dbedc3dfd92591af7c83f0c8017ca3fb1b
S-aiueo32 <18kmc06@ms.dendai.ac.jp>
2019-08-20 13:29:33 +0900

    :construction: とりあえずプッシュ

--- train.py
+++ train.py
@@ -1,12 +1,34 @@
+import argparse
+
 from pytorch_lightning.models.trainer import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from test_tube import Experiment
 
-from model import SRCNNModel
+from models import get_model
 
 
-def main():
-    model = SRCNNModel()
-    trainer = Trainer()
+def main(opt):
+    exp = Experiment(save_dir=f'./logs/{opt.model}')
+
+    exp.argparse(opt)
+    exp.tag({'description': opt.model})
+
+    model = get_model(opt)
+
+    trainer = Trainer(experiment=exp,
+                      max_nb_epochs=4000,
+                      check_val_every_n_epoch=10,
+                      gpus=[0])
+
     trainer.fit(model)
 
 if __name__ == "__main__":
-    main()
+    args = argparse.ArgumentParser()
+    args.add_argument('--model', choices=['srcnn', 'srgan'], required=True)
+    args.add_argument('--dataroot', type=str, default='./data/DIV2K')
+    args.add_argument('--scale_factor', type=int, default=4)
+    args.add_argument('--batch_size', type=int, default=16)
+    args.add_argument('--gpus', )
+    opt = args.parse_args()
+
+    main(opt)
